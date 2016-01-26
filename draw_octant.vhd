899dc7ff7e58530b81ecb352125fdf3e09f619b6
Michael Hart <michael.hart1994@gmail.com>
2016-01-26 16:21:02 +0000

    Added definition for process R1, untested

--- draw_octant.vhd
+++ draw_octant.vhd
@@ -39,10 +39,58 @@ BEGIN
 		
 		END PROCESS C1;
 
-  R1 : PROCESS(disable, err1, err2, xbias, init, draw, done)
-  BEGIN
+	R1 : PROCESS(disable, err1, err2, xbias, init, draw, done)
+		BEGIN
   
-  END PROCESS R1;
+			-- Only assign outputs if disable is low
+			IF disable = '0' AND done = '0' THEN
+				-- If initialising, assign outputs as follows
+				IF init = '1' THEN
+					-- Assign x, y values
+					x <= xin;
+					y <= yin;
+					xnew <= xin;
+					ynew <= yin;
+					-- Assign increment values
+					xincr <= '0';
+					yincr <= '0';
+					
+				-- else check other parameters
+				ELSE
+					IF draw = '1' THEN
+						xincr <= slv(signed(xin) - signed(x));
+						yincr <= slv(signed(yin) - signed(y));
+						xnew <= xin;
+						ynew <= yin;
+					ELSE
+						-- Check to see if done
+						IF done = '0' THEN
+						
+							IF err1 > err2 THEN
+								error <= slv(signed(error) + signed(yincr) - signed(xincr));
+								x <= slv(signed(x) + 1);
+								y <= slv(signed(y) + 1);
+							ELSIF err1 < err2 THEN
+								error <= slv(signed(error) + signed(yincr));
+								x <= slv(signed(x) + 1);
+							ELSE
+								-- Check xbias
+								IF xbias = '1' THEN
+									error <= slv(signed(error) + signed(yincr));
+									x <= slv(signed(x) + 1);
+								ELSE
+									error <= slv(signed(error) + signed(yincr) - signed(xincr));
+									x <= slv(signed(x) + 1);
+									y <= slv(signed(y) + 1);
+								END IF xbias;
+							
+							END IF err1err2;
+						
+						-- No else, as outputs are not changed if done is 1
+						END IF done;
+					END IF draw;
+				END IF init;
+			END IF disable;
+		END PROCESS R1;
 
 END ARCHITECTURE behav;
-
